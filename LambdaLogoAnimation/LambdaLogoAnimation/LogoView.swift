47606b51cbc17d930e81f66932dc09cec6589cf7
Alex Shillingford <alex.shillingford@icloud.com>
2019-08-15 15:26:00 -0600

    Create red pentagon background for Lambda logo

---
+++ LambdaLogoAnimation/LambdaLogoAnimation/LogoView.swift
@@ -0,0 +1,46 @@
+//
+//  LogoView.swift
+//  LambdaLogoAnimation
+//
+//  Created by Alex Shillingford on 8/15/19.
+//  Copyright © 2019 Alex Shillingford. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class LogoView: UIView {
+    
+    private let logoRedColor = UIColor.red
+    private let lambdaSymbolColor = UIColor.white
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.clear
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        backgroundColor = UIColor.clear
+    }
+    
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+        if let context = UIGraphicsGetCurrentContext() {
+            context.addLines(between: [CGPoint(x: 20, y: 200),
+                                       CGPoint(x: rect.maxX - 20, y: 200),
+                                       CGPoint(x: rect.maxX - 20, y: 500),
+                                       CGPoint(x: 207, y: 650),
+                                       CGPoint(x: 20, y: 500),
+                                       CGPoint(x: 20, y: 200)])
+            context.setFillColor(logoRedColor.cgColor)
+            context.fillPath()
+        }
+        
+    }
+    
+
+}
